0282b561ca7b8e7c583dd34542a7bcc8f3108316
PavanOvhal <Pavanovhal015@gmail.com>
2025-06-26 20:11:17 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import wikipedia
 import google.generativeai as genai
+import os
 
 app = Flask(__name__)
 
@@ -34,4 +35,6 @@ def ask():
     return jsonify({"response": answer})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
